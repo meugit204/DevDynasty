2ea4c6994c832dcfba505e11e9f3aae64cec2593
Ricardo Vieira <ricardo.dev.of@gmail.com>
2024-04-09 14:26:29 -0300

    new feedbacks

---
+++ assets/js/function.js
@@ -0,0 +1,22 @@
+let currentIndex = 0;
+const slides = document.querySelectorAll(".slide");
+const totalSlides = slides.length;
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.style.transform = `translateX(-${index * 100}%)`;
+  });
+  currentIndex = index;
+}
+
+function nextSlide() {
+  currentIndex = (currentIndex + 1) % totalSlides;
+  showSlide(currentIndex);
+}
+
+function prevSlide() {
+  currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
+  showSlide(currentIndex);
+}
+
+setInterval(nextSlide, 8000); // Troca de slide a cada 20 segundos
